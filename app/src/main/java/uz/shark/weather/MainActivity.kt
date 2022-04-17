20186a8bfcff0d428261f733109b195d302fde32
Abduqahhor <abugo1998@gmail.com>
2022-04-17 08:38:49 +0500

    Project structure created

--- app/src/main/java/uz/shark/weather/MainActivity.kt
+++ app/src/main/java/uz/shark/weather/MainActivity.kt
@@ -1,8 +1,10 @@
 package uz.shark.weather
 
-import uz.shark.weather.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 import uz.shark.weather.databinding.ActivityMainBinding
+import uz.shark.weather.ui.base.BaseActivity
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun setupItems() {
 
